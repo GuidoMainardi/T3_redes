0d2928e3d640b6a2ea35c3b1e3aa2bf9a3140e6c
GuidoMainardi <guido.mainardi@edu.pucrs.br>
2021-06-08 16:41:02 -0300

    utility

--- simulador.py
+++ simulador.py
@@ -1,57 +1,12 @@
-def imprime(list):
-    for elem in list:
-        elem.imprime()
-        print()
-
-def parser(file):
-    arq = open(file, "r").read()
-
-    n = arq.split('#')[1].split('\n')[1:-1]
-    r = arq.split('#')[2].split('\n')[1:-1]
-    t = arq.split('#')[3].split('\n')[1:]
-
-    nodes = []
-    for elem in n:
-        node_info = elem.split(',')
-        name = node_info[0]
-        MAC = node_info[1]
-        IP = node_info[2]
-        gateway = node_info[3]
-        nodes.append(Node.Node(name, MAC, IP, gateway))
-
-    routers = []
-    for elem in r:
-        table_info = elem.split(',')
-        router_name = table_info[0]
-        num_ports = table_info[1]
-        IPs = []
-        MACs = []
-        for i in range(2, len(table_info), 2):
-            MACs.append(table_info[i])
-            IPs.append(table_info[i+1])
-        routers.append(Router.Router(name, router_name, IPs, MACs))
-
-
-    tables = []
-    for elem in t:
-        table_info = elem.split(',')
-        name = table_info[0]
-        net_dest = table_info[1]
-        nexthop = table_info[2]
-        port = table_info[3]
-        tables.append(Router_table.Router_table(name, net_dest, nexthop, port))
-
-    return nodes, routers, tables
-
-import Node, Router, Router_table
+import utility
 import sys
 
 file = sys.argv[1]
 instruction = sys.argv[2]
 params = sys.argv[3:]
 
-nodes, routers, tables = parser(file)
+nodes, routers, tables = utility.parser(file)
 
-imprime(nodes)
-imprime(routers)
-imprime(tables)
+utility.imprime(nodes)
+utility.imprime(routers)
+utility.imprime(tables)
---
+++ utility.py
@@ -0,0 +1,46 @@
+import Node, Router, Router_table
+
+def imprime(list):
+    for elem in list:
+        elem.imprime()
+        print()
+
+def parser(file):
+    arq = open(file, "r").read()
+
+    n = arq.split('#')[1].split('\n')[1:-1]
+    r = arq.split('#')[2].split('\n')[1:-1]
+    t = arq.split('#')[3].split('\n')[1:]
+
+    nodes = []
+    for elem in n:
+        node_info = elem.split(',')
+        name = node_info[0]
+        MAC = node_info[1]
+        IP = node_info[2]
+        gateway = node_info[3]
+        nodes.append(Node.Node(name, MAC, IP, gateway))
+
+    routers = []
+    for elem in r:
+        table_info = elem.split(',')
+        router_name = table_info[0]
+        num_ports = table_info[1]
+        IPs = []
+        MACs = []
+        for i in range(2, len(table_info), 2):
+            MACs.append(table_info[i])
+            IPs.append(table_info[i+1])
+        routers.append(Router.Router(name, router_name, IPs, MACs))
+
+
+    tables = []
+    for elem in t:
+        table_info = elem.split(',')
+        name = table_info[0]
+        net_dest = table_info[1]
+        nexthop = table_info[2]
+        port = table_info[3]
+        tables.append(Router_table.Router_table(name, net_dest, nexthop, port))
+
+    return nodes, routers, tables
